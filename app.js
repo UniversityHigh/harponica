ecb0b5d5d4b4087082513bd8ff07b9275d33da22
Spencer Berenson <spencer@berenson.me>
2017-04-14 22:28:50 -0400

    Added directory assumptions to harponica compile.

--- app.js
+++ app.js
@@ -3,46 +3,45 @@ const Server = require("./server.js").Server;
 const fs = require("fs");
 const args = process.argv.splice(process.execArgv.length + 2);
 
-let subArgs;
-let server;
-switch(args[0].toLowerCase()) {
-	case "start":
-		if(server) throw new Error("Server instance already running.");
+if(args[0]) {
+	let subArgs = args.slice(1);
+	let server;
+	switch(args[0].toLowerCase()) {
+		case "start":
+			if(server) console.error("Server instance already running.");
 
-		subArgs = args.slice(1);
-		if(subArgs.length > 2 || subArgs.length < 1) {
-			console.error("Usage: harponica start [directory] [port]");
-		} else if(subArgs.length === 1) {
-			server = new Server(subArgs[0]);
-			server.start(3000);
-		} else if(subArgs.length === 2) {
-			server = new Server(subArgs[0]);
-			server.start(subArgs[1]);
-		}
-		break;
-	case "stop":
-		if(!server) throw new Error("Server instance not running.");
-
-		subArgs = args.slice(1);
-
-		if(subArgs.length != 0) {
-			console.error("Usage: harponica stop");
-		} else {
-			server.stop();
-		}
-		break;
-	case "compile":
-		subArgs = args.slice(1);
-
-		if(subArgs.length != 2) {
-			console.error("Usage: harponica compile <sourceDirectory> <outputDirectory>");
-		} else {
-			if(!server) {
+			if(subArgs.length > 2 || subArgs.length < 1) {
+				console.error("Usage: harponica start [directory] [port]");
+			} else if(subArgs.length === 1) {
 				server = new Server(subArgs[0]);
+				server.start(3000);
+			} else if(subArgs.length === 2) {
+				server = new Server(subArgs[0]);
+				server.start(subArgs[1]);
+			}
+			break;
+		case "stop":
+			if(!server) console.error("Server instance not running.");
+
+			if(subArgs.length != 0) {
+				console.error("Usage: harponica stop");
+			} else {
+				server.stop();
+			}
+			break;
+		case "compile":
+			if(subArgs.length === 0) {
+				if(!server) server = new Server(__dirname);
+				server.compile(path.join(__dirname, "compiled"));
+			} else if(subArgs.length === 1) {
+				if(!server) server = new Server(subArgs[0]);
+				server.compile(path.join(__dirname, "compiled"));
+			} else if(subArgs.length === 2) {
+				if(!server) server = new Server(subArgs[0]);
 				server.compile(subArgs[1]);
 			} else {
-				server.compile(subArgs[1]);
+				console.error("Usage: harponica compile <sourceDirectory> <outputDirectory>");
 			}
-		}
-		break;
+			break;
+	}
 }
