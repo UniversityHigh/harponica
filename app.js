3f4b8f2544c98d5eee360e3b22cb561005b474fd
Spencer Berenson <spencer@berenson.me>
2017-04-14 20:51:44 -0400

    Data structure changes.

--- app.js
+++ app.js
@@ -14,44 +14,44 @@ class Server {
 
 		this.server.set("views", directory);
 		this.server.set("view engine", "pug");
-		this.server.use("/assets", express.static(this.directory + "/assets"));
+		this.server.use("/assets", express.static(path.join(this.directory, "/assets")));
 		
 		this.server.get("/", (req, res) => {
-			res.render("index", merge(require(`${this.directory}/_harp.json`).globals, require(`${this.directory}/_data.json`).index));
+			res.render("index", merge(require(path.join(this.directory, "/_globals.json")), require(path.join(this.directory, "/_locals.json")).index));
 		});
 		this.server.get("/:page", (req, res) => {
-			res.render(req.params.page, merge(require(`${this.directory}/_harp.json`).globals, require(`${this.directory}/_data.json`)[req.params.page]));
+			res.render(req.params.page, merge(require(path.join(this.directory, "/_globals.json")), require(path.join(this.directory, "/_locals.json"))[req.params.page]));
 		});
 	}
 
 	start(port, callback) {
 		this.server = this.server.listen(port);
-		callback();
+		if(callback) callback();
 	}
 
 	stop(callback) {
 		this.server.close();
-		callback();
+		if(callback) callback();
 	}
 
 	compile(outputDirectory, callback) {
 		mkdirp(outputDirectory, (err) => { if(err) throw err; });
 
-		glob(`${this.directory}/[^_]*.pug`, (err, files) => {
+		glob(path.join(this.directory, "/[^_]*.pug"), (err, files) => {
 			for(let file of files) {
 				let base = path.basename(file);
 				let baseName = base.split(".")[0];
 				let baseExt = base.split(".")[1];
 
 				let compiled = pug.compileFile(file);
-				fs.writeFile(`${outputDirectory}/${baseName}.html`, compiled(merge(require(`${this.directory}/_harp.json`).globals, require(`${this.directory}/_data.json`)[baseName])), (err) => {
+				fs.writeFile(path.join(outputDirectory, `/${baseName}.html`), compiled(merge(require(path.join(this.directory, "/_globals.json")), require(path.join(this.directory, "/_locals.json"))[baseName])), (err) => {
 					if(err) throw err;
 				});
 			}
 		});
 
-		ncp(`${this.directory}/assets/img/`, `${outputDirectory}/img`, (err) => { if(err) throw err; });
-		callback();
+		ncp(path.join(this.directory, "/assets"), path.join(outputDirectory, "/assets"), (err) => { if(err) throw err; });
+		if(callback) callback();
 	}
 }
 
