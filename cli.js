ec0fe201fd16f559e4806e8cb25c77199e3241c4
Spencer Berenson <spencer@berenson.me>
2017-04-15 20:19:52 -0400

    Fixed for Mac OS.

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-const Server = require("./server.js");
 
+const Server = require("./server.js");
 const fs = require("fs");
 const path = require("path");
 const args = process.argv.splice(process.execArgv.length + 2);
